f6a6db30c545c83e1d136c0ea3ea7185be07ce1d
Elzy🐧 <elzyproot@protonmail.com>
2026-02-28 08:50:48 +0700

    Rename config_ui to ui in about.py

--- lib/core/commands/about.py
+++ lib/core/commands/about.py
@@ -1,12 +1,13 @@
+
 # MIT License.
 # Copyright (c) 2026 Storm Framework
 
 # See LICENSE file in the project root for full license information.
 
 
-import app.base.config_ui as config_ui
+import app.base.config_ui as ui
 
 
 def execute(args, context):
-    config_ui.show_about()
+    ui.show_about()
     return context

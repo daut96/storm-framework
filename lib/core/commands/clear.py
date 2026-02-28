e6b62da7ed7fed3fc77e6fb0e37cc56a632346ba
Elzy🐧 <elzyproot@protonmail.com>
2026-02-28 08:52:15 +0700

    Refactor import alias for config_ui to ui

--- lib/core/commands/clear.py
+++ lib/core/commands/clear.py
@@ -6,7 +6,7 @@
 
 import os
 from app.banners.banner import get_random_banner
-import app.base.config_ui as config_ui
+import app.base.config_ui as ui
 
 
 def execute(args, context):
@@ -14,5 +14,5 @@ def execute(args, context):
     os.system("cls" if os.name == "nt" else "clear")
     # Redisplay tool identity
     print(get_random_banner())
-    config_ui.stormUI()
+    ui.stormUI()
     return context

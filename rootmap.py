580fd844f3c6fa2f691ffa753709318c11d9f332
Elzy🐧 <218202059+storm-os@users.noreply.github.com>
2026-03-03 23:44:18 +0700

    Update rootmap.py (#15)

--- rootmap.py
+++ rootmap.py
@@ -1,17 +1,27 @@
 # MIT License.
 # Copyright (c) 2026 Storm Framework
-
 # See LICENSE file in the project root for full license information.
-
-
 import sys
 from pathlib import Path
+
+#
+# rootmap best jump shortcut to find root folder
+# make sure to use it if needed to access files in other folders
+#
 def find_and_inject_root():
     p = Path(__file__).resolve()
     for parent in p.parents:
         if (parent / '.git').exists():
             if str(parent) not in sys.path:
                 sys.path.insert(0, str(parent))
+                
             return parent
+            
     return None
+    
 ROOT = find_and_inject_root()
+###
+# directly use example:
+# from rootmap import ROOT
+# root = os.path.join(ROOT, "")
+###

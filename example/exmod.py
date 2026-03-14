ea1142a0357b51eb4b424b4adcf2964c3c0f71f4
エルジー🐧 <elzyproot@protonmail.com>
2026-03-15 01:44:18 +0700

    Enhance module instructions and add emoji
    Added emoji to the response message and provided instructions for adding Rust modules.

--- example/exmod.py
+++ example/exmod.py
@@ -69,5 +69,32 @@ because basically the name of the output binary file follows the name of the ori
 
 If there is anything else you want to ask and it is not in Storm's explanation
 just email me directly at: elzyproot@protonmail.com
-I will definitely respond if I have time hehe
+I will definitely respond if I have time hehe😄
+
+
+IMPORTANT!
+
+The current compilers in Storm are only for C, Go, and Rust
+you can check in the path scripts/cpl if you want to add for other languages
+or you want to add more efficient code it will be very helpful for sure
+
+If you add a module in Rust, make sure you create a Cargo.toml file next to it
+in the same folder as the module you created, the compiler will detect it later
+Example of the contents of Cargo.toml
+
+[package]
+name = "storm"
+version = "0.1.0"
+edition = "2024"
+
+[[bin]]
+name = "sakura" this is the name of the binary output.
+path = "sakura.rs" this is your module name
+
+[dependencies]
+aes = "0.8"
+
+After that you can check the path lib/smf/core/db/Cargo.toml
+and make sure the dependencies you need are there too
+because it's where all the Rust dependencies for offline builds are located.
 """

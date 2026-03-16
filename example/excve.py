c7b9344d76a4d768052328475d023b2b30abf5ef
エルジー🐧 <elzyproot@protonmail.com>
2026-03-16 20:04:26 +0700

    Enhance comments for CVE template usage
    Added comments to clarify CVE file structure and usage.

--- example/excve.py
+++ example/excve.py
@@ -1,4 +1,10 @@
+#
 # modules/vulnerability/category/name_cve.py
+# This is the CVE template and its place is in the top path
+# Use the appropriate CVE name to name the .py file and format
+# if the existing CVE category does not match the one you want to add
+# just create a new folder with the appropriate category name
+
 CVE_INFO = {
     "cve": "CVE-XXXX-XXXX",
     "name": "Vulnerability name",

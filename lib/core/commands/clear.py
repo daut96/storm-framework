9f8915247c474aef3f0737f1e57881db2f0a7fea
エルジー🐧 <elzyproot@protonmail.com>
2026-04-03 20:42:57 +0700

    Update clear.py

--- lib/core/commands/clear.py
+++ lib/core/commands/clear.py
@@ -1,9 +1,12 @@
+# -- https://github.com/StormWorld0/storm-framework
+# -- SMF License
 import os
 import app.base.config_ui as ui
-
 from app.banners.uib import banner
-
-
+# The clear command is used to clear the interface line history
+# so that it looks clean like it was just opened without deleting previously used data.
+###
+# Tidiness is everything
 def execute(args, context):
     # Clean the screen according to the OS
     os.system("cls" if os.name == "nt" else "clear")

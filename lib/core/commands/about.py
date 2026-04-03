4dbdebfe874e569eabb5838eff8de2fea4c655f1
エルジー🐧 <elzyproot@protonmail.com>
2026-04-03 20:32:23 +0700

    Update about.py

--- lib/core/commands/about.py
+++ lib/core/commands/about.py
@@ -1,6 +1,10 @@
 import app.base.config_ui as ui
 
-
+# -- https://github.com/StormWorld0/storm-framework/app/base/config_ui
+#
+# The about command is used to display developer information, etc.
+# to find out who the creator, contributor, version, etc.
+# This is useful for very specific information.
 def execute(args, context):
     ui.show_about()
     return context

c8e13fb23220a65025406e60752d7565533f8741
エルジー🐧 <elzyproot@protonmail.com>
2026-04-03 20:59:36 +0700

    Update search.py

--- lib/core/commands/search.py
+++ lib/core/commands/search.py
@@ -1,6 +1,12 @@
+# -- https://github.com/StormWorld0/storm-framework 
+# -- SMF License 
 from app.utility.search import search_modules
 
-
+# Search command to search for the modules we want to search for
+# This is very dynamic as in general it does not require specific words.
+# if there is a module name scan_a, scan_b, and you run it like;
+# Command => search scan
+# then all module names containing the word scan will appear completely.
 def execute(args, context):
     query = args[0] if args else ""
     if not query:

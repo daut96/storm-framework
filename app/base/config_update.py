2eade05dc245ce104dea7c04bc5434c86b5ac4e0
エルジー🐧 <elzyproot@protonmail.com>
2026-04-07 07:40:14 +0700

    Update config_update.py

--- app/base/config_update.py
+++ app/base/config_update.py
@@ -1,29 +1,21 @@
 import requests
-import re
-from version import VERSION
+import json
 from app.utility.colors import C
 
 
 def check_update():
     url = (
-        "https://raw.githubusercontent.com/StormWorld0/storm-framework/main/version.py"
+        "https://raw.githubusercontent.com/StormWorld0/storm-framework/main/version.json"
     )
     try:
-        response = requests.get(url, timeout=5)
-        response.raise_for_status()
-
-        match = re.search(r'VERSION\s*=\s*["\']([^"\']+)["\']', response.text)
-
-        if match:
-            latest = match.group(1)
-
-            remote_v = tuple(map(int, latest.split(".")))
-            local_v = tuple(map(int, VERSION.split(".")))
-
-            if remote_v > local_v:
-                print(f"{C.SUCCESS}[!] Current version => v{VERSION}")
-                print(f"{C.SUCCESS}[!] Update available => v{latest}")
-                print(f"{C.SUCCESS}[-] Type => storm update")
-                print()
+        latest_version = requests.get(url).json()['version']
+        with open('version.json') as f:
+            VERSION = json.load(f)['version']
+    
+        if latest_version > VERSION:
+            print(f"{C.SUCCESS}[!] Current version => v{VERSION}")
+            print(f"{C.SUCCESS}[!] Update available => v{latest_version}")
+            print(f"{C.SUCCESS}[-] Type => storm update")
+            print()
     except:
         pass

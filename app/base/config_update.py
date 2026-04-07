0963b7faa5835fd43aa2dcdd0140ffe95aa6fe62
エルジー🐧 <elzyproot@protonmail.com>
2026-04-07 07:02:27 +0700

    Update config_update.py

--- app/base/config_update.py
+++ app/base/config_update.py
@@ -1,21 +1,27 @@
 import requests
-from versi import VERSION
+import re
+from version import VERSION
 from app.utility.colors import C
 
 
 def check_update():
-    # Raw URL to the version.txt file on GitHub
-    url = (
-        "https://raw.githubusercontent.com/StormWorld0/storm-framework/main/version.txt"
-    )
+    url = "https://raw.githubusercontent.com/StormWorld0/storm-framework/main/version.py"
     try:
         response = requests.get(url, timeout=5)
-        latest_version = response.text.strip()
-        # If the version on GitHub is higher than the local version
-        if latest_version > VERSION:
-            print(f"{C.SUCCESS}[!] Current version => v{VERSION}")
-            print(f"{C.SUCCESS}[!] Update available => v{latest_version}")
-            print(f"{C.SUCCESS}[-] Type => storm update")
-            print("")
+        response.raise_for_status()
+        
+        match = re.search(r'VERSION\s*=\s*["\']([^"\']+)["\']', response.text)
+        
+        if match:
+            latest = match.group(1)
+            
+            remote_v = tuple(map(int, latest.split('.')))
+            local_v = tuple(map(int, VERSION.split('.')))
+            
+            if remote_v > local_v:
+                print(f"{C.SUCCESS}[!] Current version => v{VERSION}")
+                print(f"{C.SUCCESS}[!] Update available => v{latest}")
+                print(f"{C.SUCCESS}[-] Type => storm update")
+                print()
     except:
         pass

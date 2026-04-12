a153eeb6fcf3c742397915ef25914d71f25c8b90
エルジー🐧 <elzyproot@protonmail.com>
2026-04-12 08:36:51 +0700

    Update plugin.py

--- lib/core/commands/plugin.py
+++ lib/core/commands/plugin.py
@@ -1,9 +1,18 @@
 # -- https://github.com/StormWorld0/storm-framework
 # -- SMF License
-from apps.utility.plugin.utils_plugin import PluginEngine
+from apps.utility.plugin import utils_plugin as upl
 from apps.utility.colors import C
 
-
+# The plugin command is used to run the plugin manually
+# and here's how to use it
+# command => plugin <name_plugin>
+#
+# Simple example:
+# command => plugin example
+#
+# This manual logic only runs in the season when the storm is running
+# it will not be permanent if you (exit) the plugin automatically False
+# and you need to rerun it every time you run Storm.
 def execute(args, context):
     cmd = args[0].lower() if args else ""
 
@@ -11,9 +20,7 @@ def execute(args, context):
         print(f"{C.ERROR}[!] ERROR => Not module selected")
         print()
         return context
-
     else:
-        up = PluginEngine()
-        up.run_plugin(cmd)
+        upl.run_plugin(cmd)
 
     return context

c6ae8f05b9068118c9d72669a09d0f65b3ecfcb3
エルジー🐧 <elzyproot@protonmail.com>
2026-04-12 22:07:00 +0700

    Update colors.py

--- apps/utility/colors.py
+++ apps/utility/colors.py
@@ -1,5 +1,6 @@
-from colorama import Fore, Style, init
+import readline # noqa: F401
 
+from colorama import Fore, Style, init
 init(autoreset=True)
 
 
@@ -13,3 +14,13 @@ class C:
     RESET = Style.RESET_ALL
 
     BLUE = Fore.BLUE + Style.BRIGHT
+
+class CC:
+    START = "\001"
+    END = "\002"
+
+    BLUE = f"{START}{C.BLUE}{END}"
+    CYAN = f"{START}{C.MENU}{END}"
+    YELLOW = f"{START}{C.INPUT}{END}"
+    RED = f"{START}{C.ERROR}{END}"
+    RESET = f"{START}{C.RESET}{END}"

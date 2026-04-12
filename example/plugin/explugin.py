605ebc34fae72db5ede1d96038928eb91c3ce41d
エルジー🐧 <elzyproot@protonmail.com>
2026-04-12 10:35:03 +0700

    Update explugin.py

--- example/plugin/explugin.py
+++ example/plugin/explugin.py
@@ -1,12 +1,26 @@
-# Lokasi: plugin/
+# -- https://github.com/StormWorld0/storm-framework
+# -- SMF License
 
+# Use this import when you want to use the plugin
 from apps.utility.plugin.utils_plugin import plugin
 
-
+# This is an example or template of how to apply the plugin
+# to a logic module, feature, or whatever.
 def examples():
-    # Ambil objek pluginnya
+    # Get plugin name
     func = plugin("plugin_name")
 
+    # Get function inside plugin
     if func:
-        # Sekarang kamu bisa akses apapun yang ada di dalam file plugin itu
         func.plugin_function()
+
+
+
+######
+#
+#
+#
+#
+def plugin(context):
+    if context.get("event") == "run":
+        print("success")

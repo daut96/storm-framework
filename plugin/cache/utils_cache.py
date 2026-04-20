fad7711a6cdf8fc41ae795d4196c7a95b495c559
エルジー🐧 <elzyproot@protonmail.com>
2026-04-20 07:05:00 +0700

    Update utils_cache.py

--- plugin/cache/utils_cache.py
+++ plugin/cache/utils_cache.py
@@ -7,7 +7,7 @@ from rootmap import ROOT
 class StormSmartCache:
     def __init__(self):
         self.db_path = os.path.join(
-            ROOT, "lib", "core", "sf", "cache", "storm_cache.db"
+            ROOT, "lib", "smf", "core", "sf", "cache", "storm_cache.db"
         )
         self.modules_dir = os.path.join(
             ROOT, "modules"

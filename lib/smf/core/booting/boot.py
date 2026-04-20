c53427590b4a38b7eaec5c93aa95882926f8444c
エルジー🐧 <elzyproot@protonmail.com>
2026-04-20 07:03:23 +0700

    Update boot.py

--- lib/smf/core/booting/boot.py
+++ lib/smf/core/booting/boot.py
@@ -5,11 +5,19 @@ import time
 
 from apps.utility.verify import *
 from apps.utility.colors import C
-
+from plugin.cache.utils_cache import StormSmartCache
 
 def boot():
+    # Smart Cache func call
+    sync = StormSmartCache()
+    # Check core startup security
     check_critical_files()
+    # Verify file integrity
     run_verif()
+    # sync cache modules
+    sync.sync_modules()
+
+    # Countdown to pause and start
     try:
         for i in range(6, 0, -1):
             sys.stdout.write(

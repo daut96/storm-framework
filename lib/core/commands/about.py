81081012e7032379e1d1e38fa0281ed480887216
エルジー🐧 <elzyproot@protonmail.com>
2026-04-23 06:32:47 +0700

    Update about.py

--- lib/core/commands/about.py
+++ lib/core/commands/about.py
@@ -1,11 +1,11 @@
 # -- https://github.com/StormWorld0/storm-framework
 # -- SMF License
 import apps.base.config_ui as ui
-
+from lib.core.engine import Context
 
 # The about command is used to display developer information, etc.
 # to find out who the creator, contributor, version, etc.
 # This is useful for very specific information.
-def execute(args, context):
+def execute(args: list[str], ctx: "Context") -> None:
     ui.show_about()
-    return context
+    

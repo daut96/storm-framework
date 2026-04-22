bc27057895ccfed0cfd4fc624713c3a152195c12
エルジー🐧 <elzyproot@protonmail.com>
2026-04-23 06:47:21 +0700

    Update back.py

--- lib/core/commands/back.py
+++ lib/core/commands/back.py
@@ -1,11 +1,13 @@
 # -- https://github.com/StormWorld0/storm-framework
 # -- SMF License
-#
+from lib.smf.core.console.engine import Context
+
 # The back command is used to exit a module that has been locked by the use command.
 # because moving between modules is very flexible, back will not be used
 # unless you want to see global options.
-def execute(args, context):
-    if context["current_module"]:
-        context["current_module"] = None
-        context["current_module_name"] = ""
-    return context
+def execute(args: list[str], ctx: Context) -> None:
+    
+    # Checking object attributes, not key dictionary
+    if ctx.current_module:
+        ctx.current_module = None
+        ctx.current_module_name = ""

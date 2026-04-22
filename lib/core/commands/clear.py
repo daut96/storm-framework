669bfa25a5bfeb734d188f159ef2e180eb4a0f30
エルジー🐧 <elzyproot@protonmail.com>
2026-04-23 06:50:03 +0700

    Update clear.py

--- lib/core/commands/clear.py
+++ lib/core/commands/clear.py
@@ -4,16 +4,16 @@ import os
 import smf
 import apps.base.config_ui as ui
 from apps.banners.uib import *
-
+from lib.smf.core.console.engine import Context
 
 # The clear command is used to clear the interface line history
 # so that it looks clean like it was just opened without deleting previously used data.
 ###
 # Tidiness is everything
-def execute(args, context):
+def execute(args: list[str], ctx: Context) -> None:
     # Clean the screen according to the OS
     os.system("cls" if os.name == "nt" else "clear")
     # Redisplay tool identity
     smf.printf(banner_live())
     ui.stormUI()
-    return context
+    

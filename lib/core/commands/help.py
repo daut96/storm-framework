0defd17f173be9bd73979ca03cae4582bdc75200
エルジー🐧 <elzyproot@protonmail.com>
2026-04-23 06:58:58 +0700

    Update help.py

--- lib/core/commands/help.py
+++ lib/core/commands/help.py
@@ -1,10 +1,9 @@
 # -- https://github.com/StormWorld0/storm-framework
 # -- SMF License
 import apps.base.config_ui as ui
-
+from lib.smf.core.console.engine import Context
 
 # Display help to make it easier for users to see what commands are available.
 # Without this the user is confused about what commands are in storm.
-def execute(args, context):
+def execute(args: list[str], ctx: Context) -> None:
     ui.show_help()
-    return context

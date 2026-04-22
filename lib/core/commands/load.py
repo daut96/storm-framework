e5571ab5d5f4c4b91b4bec61e31690519af48be0
エルジー🐧 <elzyproot@protonmail.com>
2026-04-23 05:24:07 +0700

    Update load.py

--- lib/core/commands/load.py
+++ lib/core/commands/load.py
@@ -7,16 +7,15 @@ from lib.roar.plugin.manager import PluginManager
 
 
 def execute(args, context):
-    cmd = args[0].lower
     plugin = PluginManager()
 
-    if not cmd:
+    if not args:
         smf.printf(
             f"{CC.RED}[!] ERROR => Invalid syntax. Run help to see the correct command.{CC.RESET}"
         )
         return context
 
-    if cmd:
-        plugin.load(cmd)
+    cmd = args[0].lower
+    plugin.load(cmd)
 
     return context

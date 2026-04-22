438748c903a24e6936be3d4d5080c517cbab6550
エルジー🐧 <elzyproot@protonmail.com>
2026-04-23 06:52:43 +0700

    Update exit.py

--- lib/core/commands/exit.py
+++ lib/core/commands/exit.py
@@ -1,9 +1,9 @@
 # -- https://github.com/StormWorld0/storm-framework
 # -- SMF License
-# # #
+from lib.smf.core.console.engine import Context
 # Exit command to avoid errors or crashes in storm.
 # Because if you only use CTRL + C it is possible that the storm will come out messy.
 # This will minimize the possibility of a crash to prevent damage.
-def execute(args, context):
-    context["exit"] = True
-    return context
+def execute(args: list[str], ctx: Context) -> None:
+    # Mutate state exit in-place
+    ctx.exit = True

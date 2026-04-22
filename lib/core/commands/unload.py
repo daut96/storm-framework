728ac3f2bc1f9b7a9228c3ca1aa83190fbc28b2f
エルジー🐧 <elzyproot@protonmail.com>
2026-04-23 04:58:58 +0700

    Create unload.py

---
+++ lib/core/commands/unload.py
@@ -0,0 +1,22 @@
+# -- https://github.com/StormWorld0/storm-framework
+# -- SMF License
+import smf
+
+from apps.utility.colors import CC
+from lib.roar.plugin.manager import PluginManager
+
+
+def execute(args, context):
+    cmd = args[0].lower
+    plugin = PluginManager()
+
+    if not cmd:
+        smf.printf(
+            f"{CC.RED}[!] ERROR => Invalid syntax. Run help to see the correct command.{CC.RESET}"
+        )
+        return context
+
+    if cmd:
+        plugin.unload(cmd)
+        
+    return context

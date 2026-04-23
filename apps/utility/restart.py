01ec33c4cbf08d1afb97fc19120b205ae1657dad
エルジー🐧 <elzyproot@protonmail.com>
2026-04-23 09:03:12 +0700

    Update restart.py

--- apps/utility/restart.py
+++ apps/utility/restart.py
@@ -2,12 +2,12 @@ import os
 import sys
 import smf
 import lib.smf.core.sf.svch as svch
-from lib.smf.core.console.engine import Context
 
 
-def run_restart(ctx: Context):
+def run_restart(ctx):
+    options = ctx.options
     # save old variables
-    svch.session(ctx.options)
+    svch.session(options)
     # Restart the storm
     executable = sys.argv[0]
     args = sys.argv

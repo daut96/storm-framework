09f8970b2ad2edebb310f841d6e36592561bce55
エルジー🐧 <elzyproot@protonmail.com>
2026-04-23 07:03:18 +0700

    Update load.py

--- lib/core/commands/load.py
+++ lib/core/commands/load.py
@@ -4,18 +4,16 @@ import smf
 
 from apps.utility.colors import CC
 from lib.roar.plugin.manager import PluginManager
+from lib.smf.core.console.engine import Context
 
-
-def execute(args, context):
+def execute(args: list[str], ctx: Context) -> None:
     plugin = PluginManager()
 
     if not args:
         smf.printf(
             f"{CC.RED}[!] ERROR => Invalid syntax. Run help to see the correct command.{CC.RESET}"
         )
-        return context
+        return
 
     cmd = args[0].lower
     plugin.load(cmd)
-
-    return context

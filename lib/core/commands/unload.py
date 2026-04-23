c63d389f00eac52635ef59f57c5b530c0c121433
エルジー🐧 <elzyproot@protonmail.com>
2026-04-24 02:24:43 +0700

    Update unload.py

--- lib/core/commands/unload.py
+++ lib/core/commands/unload.py
@@ -5,14 +5,22 @@ import smf
 from apps.utility.colors import CC
 from lib.smf.core.console.engine import Context
 
-
+# This command is used to turn off the plugin life cycle.
+# active, in the following way;
+#
+# Command => unload <plugin_name>
+#
+# It will directly disable the active plugin.
 def execute(args: list[str], ctx: Context) -> None:
 
+    # Input validation
     if not args:
         smf.printf(
             f"{CC.RED}[!] ERROR => Invalid syntax. Run help to see the correct command.{CC.RESET}"
         )
         return
 
+    # Get plugin name command
     cmd = args[0].lower()
+    # Unload plugin name
     ctx.plugin.unload(cmd)

3d693981ab163d3bddb277092b1a473f9d4ae2a8
エルジー🐧 <elzyproot@protonmail.com>
2026-04-23 08:02:38 +0700

    Update unload.py

--- lib/core/commands/unload.py
+++ lib/core/commands/unload.py
@@ -4,19 +4,16 @@ import smf
 
 from apps.utility.colors import CC
 from lib.roar.plugin.manager import PluginManager
+from lib.smf.core.console.engine import Context
 
-
-def execute(args, context):
-    cmd = args[0].lower
+def execute(args: list[str], ctx: Context) -> None:
     plugin = PluginManager()
 
     if not cmd:
         smf.printf(
             f"{CC.RED}[!] ERROR => Invalid syntax. Run help to see the correct command.{CC.RESET}"
         )
-        return context
+        return
 
-    if cmd:
-        plugin.unload(cmd)
-
-    return context
+    cmd = args[0].lower
+    plugin.unload(cmd)

de1ddb5fe47b08e988dd98955e814ea1db47477c
エルジー🐧 <elzyproot@protonmail.com>
2026-04-24 01:45:13 +0700

    Update boot.py

--- lib/smf/core/booting/boot.py
+++ lib/smf/core/booting/boot.py
@@ -6,7 +6,7 @@ import time
 from apps.utility.verify import *
 from apps.utility.colors import C
 from lib.roar.plugin.manager import registry as plugin
-
+from lib.roar.cache import cache_modules as cache
 
 def boot():
     # Check core startup security
@@ -15,6 +15,8 @@ def boot():
     run_verif()
     # Boot manager
     plugin.boot()
+    # Cache modules synchronization
+    cache.sync_modules
     # Countdown to pause and start
     try:
         for i in range(6, 0, -1):

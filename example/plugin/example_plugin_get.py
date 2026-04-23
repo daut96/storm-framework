a1a99962b640afcf19ae532c01d4ae0f4249c957
エルジー🐧 <elzyproot@protonmail.com>
2026-04-23 17:12:02 +0700

    Update example_plugin_get.py

--- example/plugin/example_plugin_get.py
+++ example/plugin/example_plugin_get.py
@@ -1,30 +1,16 @@
-from lib.roar.plugin.manager import PluginManager
+# Ini import yang di gunakan
+from lib.roar.plugin.manager import registry as plugin
 
 
-def run_external_logic():
-    """Simulasi komponen framework lain yang membutuhkan plugin"""
 
-    ex_plugin = PluginManager()
-    # 1. Minta instance plugin dari manager (Inversion of Control)
-    plugin = ex_plugin.get("demo_plug")
+def example():
+    # Ini untuk mangambil nama plugin yang ingin di gunakan.
+    example = plugin.get("storm")
 
-    # -- Skenario 1: Fungsi Berjalan Normal --
-    # Jika plugin belum di-load, plugin.process_data akan ditangkap
-    # oleh SilentAbsorber dan me-return None tanpa error.
-    hasil = plugin.process_data("Packet-01")
-    if hasil:
-        print(f"[Core] Hasil proses: {hasil}")
+    # Ini untuk mengambil nama fungsi secara spesifik.
+    example.plugin.storm_fucn("input user")
 
-    # -- Skenario 2: Fungsi Memicu Crash (Runtime Error) --
-    # Jika caller memanggil fungsi yang rusak, framework TETAP HIDUP.
-    # Error (ZeroDivisionError) akan dikirim ke Rust logger, dan
-    # method ini diam-diam me-return None.
-    plugin.trigger_crash()
-
-    # -- Skenario 3: Memanggil fungsi yang tidak pernah dibuat --
-    # Caller berhalusinasi dan memanggil objek berantai yang tidak ada.
-    # SilentAbsorber akan menyerap .database, .connect(), dan .fetch().
-    # Tidak ada AttributeError maupun TypeError yang bocor ke thread utama.
-    plugin.database.connect().fetch("SELECT *")
-
-    print("[Core] Logika luar selesai dieksekusi dengan aman.")
+    # Ini langsung memasukkan string untuk plugin
+    # tanpa tau function plugin.
+    # tapi agak lambat karena harus melakukan pencarian otomatis di sisi mesin.
+    example.function("input user")

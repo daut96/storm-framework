8b1f3fee0cac6cd581b30fc72c9d78066a9fd8de
エルジー🐧 <elzyproot@protonmail.com>
2026-04-23 07:13:27 +0700

    Update search.py

--- lib/core/commands/search.py
+++ lib/core/commands/search.py
@@ -3,17 +3,16 @@
 import smf
 from apps.utility.colors import C
 from apps.utility.search import search_modules
-
+from lib.smf.core.console.engine import Context
 
 # Search command to search for the modules we want to search for
 # This is very dynamic as in general it does not require specific words.
 # if there is a module name scan_a, scan_b, and you run it like;
 # Command => search scan
 # then all module names containing the word scan will appear completely.
-def execute(args, context):
+def execute(args: list[str], ctx: Context) -> None:
     query = args[0] if args else ""
     if not query:
         smf.printf(f"{C.INPUT}[-] Enter file name to search!")
     else:
         search_modules(query)
-    return context

fad12aa4c55ee12a10c74fb51a9f6a349c305056
エルジー🐧 <elzyproot@protonmail.com>
2026-04-24 04:46:20 +0700

    Update example_plugin_get.py

--- example/plugin/example_plugin_get.py
+++ example/plugin/example_plugin_get.py
@@ -1,15 +1,21 @@
 # Ini import yang di gunakan
-from lib.roar.plugin.manager import registry as plugin
+from lib.roar.plugins import register as app
 
 
 def example():
     # Ini untuk mangambil nama plugin yang ingin di gunakan.
-    example = plugin.get("storm")
+    example = app.plugins.get("storm")
 
     # Ini untuk mengambil nama fungsi secara spesifik.
-    example.plugin.storm_fucn("input user")
+    example.plugins.storm_fucn("input user")
+    # Atau
+    example.plugins.storm_fucn(storm)
 
     # Ini langsung memasukkan string untuk plugin
     # tanpa tau function plugin.
     # tapi agak lambat karena harus melakukan pencarian otomatis di sisi mesin.
-    example.function("input user")
+    example.plugins.function("input user")
+    # Atau
+    example.plugins.function(storm)
+
+    app.plugins.broadcast("saat_masukan_pengguna_diterima", payload="input user")

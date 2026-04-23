0af8636d020a323f2f955f9a1679a8495a31879b
エルジー🐧 <elzyproot@protonmail.com>
2026-04-23 08:28:50 +0700

    Update unload.py

--- lib/core/commands/unload.py
+++ lib/core/commands/unload.py
@@ -10,7 +10,7 @@ from lib.smf.core.console.engine import Context
 def execute(args: list[str], ctx: Context) -> None:
     plugin = PluginManager()
 
-    if not cmd:
+    if not args:
         smf.printf(
             f"{CC.RED}[!] ERROR => Invalid syntax. Run help to see the correct command.{CC.RESET}"
         )

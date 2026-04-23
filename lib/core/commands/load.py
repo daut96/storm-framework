59cd1afa514a548f2184e87abb6084acc073b842
エルジー🐧 <elzyproot@protonmail.com>
2026-04-24 02:29:26 +0700

    Update load.py

--- lib/core/commands/load.py
+++ lib/core/commands/load.py
@@ -5,14 +5,23 @@ import smf
 from apps.utility.colors import CC
 from lib.smf.core.console.engine import Context
 
-
+# This command is used to activate the plugin.
+# by loading it into memory manually
+# so that it can be immediately activated and used by the machine.
+#
+# Command => load <plugin_name>
+#
+# It will automatically load the plugin into memory.
 def execute(args: list[str], ctx: Context) -> None:
 
+    # Input validation 
     if not args:
         smf.printf(
             f"{CC.RED}[!] ERROR => Invalid syntax. Run help to see the correct command.{CC.RESET}"
         )
         return
 
+    # Get plugin name from input
     cmd = args[0].lower()
+    # Load plugin into memory
     ctx.plugin.load(cmd)

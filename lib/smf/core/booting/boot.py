9fd63fffa79012c6a0bacda3b0b53f491171582d
エルジー🐧 <elzyproot@protonmail.com>
2026-05-01 20:59:41 +0700

    Implement binary synchronization in boot process
    Added binary synchronization step to the boot process.

--- lib/smf/core/booting/boot.py
+++ lib/smf/core/booting/boot.py
@@ -7,23 +7,32 @@ import smf
 from apps.utility.verify import *
 from lib.roar.plugin_api import plugin
 from lib.roar.cache import cache_modules as cache
-
+from lib.roar.callbin import calling
 
 def boot():
     smf.printd("Boot starting...", level="INFO")
     smf.printd("Start checking the core...", level="INFO")
+
     # Check core startup security
     check_critical_files()
     smf.printd("Check core success", level="INFO")
+
     # Boot Plugin Manager
     plugin.boot()
     smf.printd("Boot plugin successfuls", level="INFO")
+
     # Cache modules synchronization
     cache.sync_modules()
     smf.printd("Module synchronization successful", level="INFO")
+
+    # Cache binary synchronization
+    calling.sync_bin()
+    smf.printd("Binary synchronization successful", level="INFO")
+
     # Verify file integrity
     run_verif()
     smf.printd("Verification of integrity check success", level="INFO")
+        
     # Countdown to pause and start
     try:
         for i in range(6, 0, -1):

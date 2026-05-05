da615c62693ce58024e80696726841f1a8ff5441
エルジー🐧 <elzyproot@protonmail.com>
2026-05-06 03:56:49 +0700

    Update sec-ch-ua-platform header to Android

--- modules/auxiliary/test_waf_evasion.py
+++ modules/auxiliary/test_waf_evasion.py
@@ -26,7 +26,7 @@ def execute(options):
         "accept-language": "en-US,en;q=0.9",
         "sec-ch-ua": '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
         "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": '"?0"',
+        "sec-ch-ua-platform": '"Android"',
         "sec-fetch-dest": "document",
         "sec-fetch-mode": "navigate",
         "sec-fetch-site": "none",

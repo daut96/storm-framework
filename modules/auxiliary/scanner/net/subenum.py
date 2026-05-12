5e95ed312a12b6f013582973431970c503c84935
エルジー🐧 <elzyproot@protonmail.com>
2026-05-12 08:16:05 +0700

    Enhance subenum.py with example options and logging
    Updated required options with example values and improved logging messages.

--- modules/auxiliary/scanner/net/subenum.py
+++ modules/auxiliary/scanner/net/subenum.py
@@ -19,7 +19,7 @@ exploited.
     "DefaultAction": "Scanner",
     "License": "SMF License",
 }
-REQUIRED_OPTIONS = {"DOMAIN": "", "SUBDOM": "", "THREAD": ""}
+REQUIRED_OPTIONS = {"DOMAIN": "ex: google.com", "SUBDOM": "subdomain", "THREAD": "100"}
 
 
 def execute(options):
@@ -30,10 +30,10 @@ def execute(options):
     binary = call_bin("subenum")
 
     if not binary:
-        smf.printf(f"[!] ERROR => Binary not found at >", binary)
+        smf.printf(f"{CC.YELLOW}[!] WARN => Binary not found at >{CC.RESET}", binary)
         return
 
-    smf.printf(f"\n[*] Starting SUBDOMAIN ENUMERATION for", target_domain)
+    smf.printf(f"\n{CC.YELLOW}[*] Starting SUBDOMAIN ENUMERATION for {target_domain}{CC.RESET}")
     smf.printf()
 
     cmd = [binary, "-d", target_domain, "-w", wordlist_path, "-c", threads]
@@ -42,19 +42,19 @@ def execute(options):
         process = subprocess.Popen(
             cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, bufsize=1
         )
-
-        # Thread untuk parsing hasil valid (stdout)
+        
+        # Thread for parsing valid results (stdout)
         def read_stdout(pipe):
             for line in iter(pipe.readline, ""):
                 if line:
                     smf.printf(f"{CC.GREEN}[✓] {line.strip()}{CC.RESET}")
             pipe.close()
 
-        # Thread untuk parsing info/error (stderr)
+        # Thread for parsing info/error (stderr)
         def read_stderr(pipe):
             for line in iter(pipe.readline, ""):
                 if line:
-                    smf.printf(f"{line.strip()}")
+                    smf.printf(f"{CC.YELLOW}{line.strip()}{CC.RESET}")
             pipe.close()
 
         stdout_thread = threading.Thread(target=read_stdout, args=(process.stdout,))
@@ -68,5 +68,5 @@ def execute(options):
         stderr_thread.join()
 
     except Exception as e:
-        smf.printf(f"[!] An IPC module error occurred")
+        smf.printf(f"{CC.RED}[!] An IPC module error occurred{CC.RESET}")
         smf.printd("Subenum IPC error", e, level="ERROR")

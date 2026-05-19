41a8836d7d66889575cc81634781b696ffdc793c
エルジー🐧 <elzyproot@protonmail.com>
2026-05-19 16:24:47 +0700

    Clarify URL and enhance exception handling in check_update
    Added a comment to clarify the URL for the GitHub data JSON and improved the exception handling structure.

--- apps/base/config_update.py
+++ apps/base/config_update.py
@@ -8,18 +8,28 @@ from rootmap import ROOT
 
 
 def check_update():
+    # Url to github data json
     url = "https://raw.githubusercontent.com/StormWorld0/storm-framework/main/data/data_version.json"
-    try:
+    try: # Request get data json
         latest_version = requests.get(url, timeout=3).json()["version"]
+        # Get local json data
         data = os.path.join(ROOT, "data", "data_version.json")
+
+        # View contents and search for versions
         with open(data) as f:
             VERSION = json.load(f)["version"]
 
+        # Compare current version with github
         if latest_version > VERSION:
             smf.printf(f"{C.SUCCESS}[!] Current version => v{VERSION}")
             smf.printf(f"{C.SUCCESS}[!] Update available => v{latest_version}")
             smf.printf(f"{C.SUCCESS}[-] Type => storm update")
             smf.printf()
+            
+    except requests.exceptions.RequestException as e:
+        smf.printd("ERROR CONNECTION CHECK UPDATE =>", e, level="ERROR")
+        smf.printf("CONNECTION TIMEOUT CHECK UPDATE")
+        
     except Exception as e:
         smf.printd("ERROR CHECK UPDATE", e, level="ERROR")
         smf.printf(f"ERROR CHECK UPDATE")

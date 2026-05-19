a2b27edd97f7c03be954e5eb773f599e4c2e64d8
エルジー🐧 <elzyproot@protonmail.com>
2026-05-20 02:07:22 +0700

    Simplify execute function signature
    Removed type hints from the execute function.

--- lib/core/commands/about.py
+++ lib/core/commands/about.py
@@ -1,11 +1,9 @@
 # -- https://github.com/StormWorld0/storm-framework
 # -- SMF License
 import apps.base.config_ui as ui
-from lib.smf.core.console.engine import Context
-
 
 # The about command is used to display developer information, etc.
 # to find out who the creator, contributor, version, etc.
 # This is useful for very specific information.
-def execute(args: list[str], ctx: Context) -> None:
+def execute(args, ctx):
     ui.show_about()

60648e0e8d6cbbd71bdbb3d9a82f72652cf77701
エルジー🐧 <elzyproot@protonmail.com>
2026-05-20 02:09:24 +0700

    Refactor execute function signature in clear.py
    Updated the execute function signature to remove type hints.

--- lib/core/commands/clear.py
+++ lib/core/commands/clear.py
@@ -3,15 +3,15 @@
 import os
 import smf
 import apps.base.config_ui as ui
+
 from apps.banners.uib import *
-from lib.smf.core.console.engine import Context
 
 
 # The clear command is used to clear the interface line history
 # so that it looks clean like it was just opened without deleting previously used data.
 ###
 # Tidiness is everything
-def execute(args: list[str], ctx: Context) -> None:
+def execute(args, ctx):
     # Clean the screen according to the OS
     os.system("cls" if os.name == "nt" else "clear")
     # Redisplay tool identity

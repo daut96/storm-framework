9a3905dfd84a4ba094622c6c1600e93ac1405f6b
エルジー🐧 <elzyproot@protonmail.com>
2026-06-01 22:44:40 +0700

    Remove Path Enumeration action from HTTP fuzzer
    Removed 'Path Enumeration' action from the list of actions.

--- modules/auxiliary/fuzzers/http/path_enum.py
+++ modules/auxiliary/fuzzers/http/path_enum.py
@@ -19,12 +19,12 @@ a combination of a wordlist and a recursive crawler.
 """,
     "Author": ["zxelzy"],
     "Action": [
-        ["Path Enumeration", {"Description": "Perform a search on the URL path"}],
         ["Fuzzing", {"Description": "fuzz URL path list"}],
         ["Crawler", {"Description": "Crawl html for JS and dissect recursively"}],
     ],
     "DefaultAction": "Fuzzing",
     "License": "SMF License",
+    "Date": "2026-05-27",
 }
 REQUIRED_OPTIONS = {
     "URL": "",

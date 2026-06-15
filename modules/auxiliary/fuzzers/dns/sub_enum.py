75880fa0a28a1a1dc2335178bbb50dffb3b1c074
エルジー🐧 <elzyproot@protonmail.com>
2026-06-15 21:07:22 +0700

    Fix output_stream function formatting

--- modules/auxiliary/fuzzers/dns/sub_enum.py
+++ modules/auxiliary/fuzzers/dns/sub_enum.py
@@ -32,13 +32,13 @@ log_lock = threading.Lock()
 def output_stream(line: str) -> str:
     """Color log stdout"""
     if "[INFO] =>" in line:
-        return f"{CC.YELLOW}{line}{CC.RESET}\n"
+        return f"{CC.YELLOW}{line}{CC.RESET}"
 
     if "FOUND =>" in line:
         return f"[✓] {CC.GREEN}{line}{CC.RESET}"
 
     if "[*]" in line:
-        return f"\n{CC.YELLOW}{line}{CC.RESET}"
+        return f"{CC.YELLOW}{line}{CC.RESET}"
 
     return line
 

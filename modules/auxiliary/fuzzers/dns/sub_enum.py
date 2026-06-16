98d30394d41e5de7bb64206f912185dc6ffb5d6f
エルジー🐧 <elzyproot@protonmail.com>
2026-06-16 07:28:22 +0700

    Change log output format in sub_enum.py

--- modules/auxiliary/fuzzers/dns/sub_enum.py
+++ modules/auxiliary/fuzzers/dns/sub_enum.py
@@ -31,7 +31,7 @@ REQUIRED_OPTIONS = {
 def output_stream(line: str) -> str:
     """Color log stdout"""
     if "STATUS" in line:
-        return f"[INFO] {CC.YELLOW}=> {line}{CC.RESET}\n\n"
+        return f"[*] {CC.YELLOW}INFO => {line}{CC.RESET}\n\n"
 
     if "FOUND =>" in line:
         return f"[✓] {CC.GREEN}{line}{CC.RESET}"
